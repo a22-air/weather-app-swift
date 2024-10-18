957bc7276a2c7d3c134dfa97e9725f73fec8481b
谷口愛利 <taniguchi.airi1170@alphatc.co.jp>
2024-10-18 13:31:59 +0900

    追加：WeatherTableViewCellを追加（SubTableViewCellが画面崩れを起こしてしまったためカスタムセルを再作成）

---
+++ SampleCreate/WeatherTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  WeatherTableViewCell.swift
+//  SampleCreate
+//
+//  Created by taniguchi.airi on 2024/10/18.
+//
+
+import UIKit
+
+class WeatherTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
