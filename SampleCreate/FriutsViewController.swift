df945995083a26c5024b07b53d8b2e9beecf70fa
谷口愛利 <taniguchi.airi1170@alphatc.co.jp>
2024-10-18 10:16:09 +0900

    追加：カスタムセルの追加。画面の追加。

---
+++ SampleCreate/FriutsViewController.swift
@@ -0,0 +1,31 @@
+//
+//  FriutsViewController.swift
+//  SampleCreate
+//
+//  Created by taniguchi.airi on 2024/10/17.
+//
+
+import UIKit
+
+class FriutsViewController: UIViewController {
+
+    @IBOutlet weak var screenLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        screenLabel.textColor = UIColor.red
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
