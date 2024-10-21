0955813af515bd18d3f1577f78949697ec42c8dd
谷口愛利 <taniguchi.airi1170@alphatc.co.jp>
2024-10-21 17:23:50 +0900

    追加：Realmに保存するデータのプロパティを定義 （セクション1のプロパティ）

---
+++ SampleCreate/Fruit-swif.swift
@@ -0,0 +1,13 @@
+//
+//  Fruit-swif.swift
+//  SampleCreate
+//
+//  Created by taniguchi.airi on 2024/10/21.
+//
+
+import Foundation
+import RealmSwift
+
+class Fruit: Object {
+    @objc dynamic var name = ""
+}
